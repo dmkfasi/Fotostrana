e06f0bed2fd13c981ee1dadb74b304a81f90f340
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-24 22:21:34 +0300

    Properties cleanup

--- SeekableIteratorFileReader.php
+++ SeekableIteratorFileReader.php
@@ -6,119 +6,119 @@
  */
 Class SeekableIteratorFileReader implements SeekableIterator {
 
-  protected $_fileName = '';
-  protected $_blockSize = 1;
-  protected $_fileHandle = null;
-  protected $_buffer = null;
-  protected $_position = 0;
-  protected $_isValid = true;
+  protected $fileName = '';
+  protected $blockSize = 1;
+  protected $fileHandle = null;
+  protected $buffer = null;
+  protected $position = 0;
+  protected $isValid = true;
 
   public function __construct(string $name) {
     if (is_readable($name)) {
-      $this->_fileName = $name;
+      $this->fileName = $name;
     } else {
       // Invalidate pointer position immediately and throw an exception
-      $this->_isValid = false;
+      $this->isValid = false;
       throw new Exception("File {$name} does not exists or inaccessable");
     }
   }
 
   public function getBlockSize() {
-    return $this->_blockSize;
+    return $this->blockSize;
   }
 
   public function setBlockSize(int $size = 1) {
     if ($size < 1) {
       throw new Exception('Block size must be greater than 0');
     } else {
-      $this->_blockSize = $size;
+      $this->blockSize = $size;
     }
   }
 
   public function open() {
     // Open file in binary mode and seek at the very beginning
-    $this->_fileHandle = fopen($this->_fileName, 'rb');
+    $this->fileHandle = fopen($this->fileName, 'rb');
   }
 
   public function close() {
-    fclose($this->_fileHandle);
+    fclose($this->fileHandle);
   }
 
   public function readBlock() {
-    if (!feof($this->_fileHandle)) {
+    if (!feof($this->fileHandle)) {
       $this->updatePosition();
-      $this->_buffer = fread($this->_fileHandle, $this->_blockSize);
+      $this->buffer = fread($this->fileHandle, $this->blockSize);
     } else {
       return false;
     }
   }
 
   private function updatePosition() {
-    $this->_position = ftell($this->_fileHandle);
+    $this->position = ftell($this->fileHandle);
   }
 
   public function prev(): void {
     // Move pointer backward by the block size from the
     // current position and then store its location
-    if (fseek($this->_fileHandle,
-            ($this->_position - $this->_blockSize)) === 0) {
+    if (fseek($this->fileHandle,
+            ($this->position - $this->blockSize)) === 0) {
       $this->readBlock();
     } else {
       // Otherwise mark the position invalid and throw an exception
-      $this->_isValid = false;
+      $this->isValid = false;
       throw new OutOfBoundsException();
     }
   }
 
   public function current() {
-    if (empty($this->_buffer)) {
+    if (empty($this->buffer)) {
       $this->readBlock();
     }
 
-    return $this->_buffer;
+    return $this->buffer;
   }
 
   public function key(): int {
-    return $this->_position;
+    return $this->position;
   }
 
   public function next(): void {
     // Move pointer farther by the block size and then store its location
-    if (fseek($this->_fileHandle,
-            ($this->_position + $this->_blockSize)) === 0) {
+    if (fseek($this->fileHandle,
+            ($this->position + $this->blockSize)) === 0) {
       $this->readBlock();
     } else {
       // Otherwise mark the position invalid and throw an exception
-      $this->_isValid = false;
+      $this->isValid = false;
       throw new OutOfBoundsException();
     }
   }
 
   public function rewind(): void {
-    if (rewind($this->_fileHandle) === false) {
+    if (rewind($this->fileHandle) === false) {
       // Mark the position invalid and throw an exception
-      $this->_isValid = false;
+      $this->isValid = false;
       throw new Exception('Unable to rewind file position');
     } else {
       // Set new location
-      $this->_position = 0;
+      $this->position = 0;
     }
   }
 
   public function seek($position): void {
     // Move pointer to an arbitrary position multiplied
     // by the block size and then store its location
-    if (fseek($this->_fileHandle, $position * $this->_blockSize) === 0) {
-      $this->_position = $position;
+    if (fseek($this->fileHandle, $position * $this->blockSize) === 0) {
+      $this->position = $position;
     } else {
       // Otherwise mark the position invalid and throw an exception
-      $this->_isValid = false;
+      $this->isValid = false;
       throw new OutOfBoundsException("Position {$position} is out of bound");
     }
   }
 
   public function valid(): bool {
-    return $this->_isValid;
+    return $this->isValid;
   }
 
 }
