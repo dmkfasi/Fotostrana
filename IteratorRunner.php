f66ee88644cbadb347dbaa787937a59b95f10353
Andrei Ivanov <dmkfasi@gmail.com>
2019-11-24 23:00:04 +0300

    New way to test

--- IteratorRunner.php
+++ IteratorRunner.php
@@ -5,8 +5,23 @@ require 'SeekableIteratorFileReader.php';
 $sifr = new SeekableIteratorFileReader('hugetextfile.txt');
 
 $sifr->open();
-$sifr->setBlockSize(1024);
+$sifr->setBlockSize(32);
 $sifr->seek(10);
-$sifr->readBlock();
-echo $sifr->current();
+
+
+for ($i = 0; $i < 8; $i++) {
+  testPrint($sifr);
+  $sifr->next();
+}
+
+for ($i = 0; $i < 16; $i++) {
+  testPrint($sifr);
+  $sifr->prev();
+}
+
 $sifr->close();
+
+
+function testPrint(SeekableIteratorFileReader $sifr) {
+  echo "[ Current position @{$sifr->key()} | Block content: `{$sifr->current()}` ]\n";
+}
